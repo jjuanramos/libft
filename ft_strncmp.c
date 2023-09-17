6a94c020df996b6fd3d642d998a45fabaed43886
jjuanramos <jjuanramos91@gmail.com>
2023-09-17 16:27:36 +0200

    still not there

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,9 +6,11 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 12:20:40 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/17 12:30:37 by juramos          ###   ########.fr       */
+/*   Updated: 2023/09/17 16:21:05 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, int n)
 {
@@ -17,12 +19,11 @@ int	ft_strncmp(char *s1, char *s2, int n)
 	pos = 0;
 	if (n == 0)
 		return (0);
-	while (pos <= n)
+	while (pos < n)
 	{
-		if (s1[pos] != s2[pos] || s1[pos] == '\0'
-			|| s2[pos] == '\0' || pos == n)
+		if (s1[pos] != s2[pos] || pos == n - 1)
 			return (s1[pos] - s2[pos]);
 		pos++;
 	}
-	return (s1[pos] - s2[pos]);
+	return (s1[pos - 1] - s2[pos - 1]);
 }
