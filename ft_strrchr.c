5c6557d6a77872f49b3c88efc330f5300cf626e8
Juan Ramos <juramos@c2r13s1.42madrid.com>
2023-09-19 11:40:22 +0200

    another mod  of strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:59:03 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/18 13:40:42 by juramos          ###   ########.fr       */
+/*   Updated: 2023/09/19 11:37:04 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strrchr(const char *s, int c)
 	last = 0;
 	while (*s)
 	{
-		if (*s == (unsigned char)c)
+		if (*s == (char)c)
 			last = (char *)s;
 		s++;
 	}
