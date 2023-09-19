d849066b3034978ad4519d610c0c1ee66f566abb
Juan Ramos <juramos@c2r13s1.42madrid.com>
2023-09-19 14:15:37 +0200

    added placeholder

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,12 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 14:04:21 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/19 14:04:36 by juramos          ###   ########.fr       */
+/*   Updated: 2023/09/19 14:15:18 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-
 }
