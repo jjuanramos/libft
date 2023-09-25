1731b550e8d659ffa3bbecac1e8b2f819bbe6987
jjuanramos <jjuanramos91@gmail.com>
2023-09-25 17:02:26 +0200

    made them work

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:11:10 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/25 14:16:05 by juramos          ###   ########.fr       */
+/*   Updated: 2023/09/25 16:57:33 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,12 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	t_list	*val;
-	int		len;
+	size_t	len;
 
 	len = 0;
-	if (!lst)
-		return (0);
-	if (!lst->next)
-		return (1);
-	val = lst->next;
-	while (val->next)
+	while (lst && ++len)
 	{
-		val = val->next;
-		len++;
+		lst = lst->next;
 	}
 	return (len);
 }
