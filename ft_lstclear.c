ca709f9dfb69a8cafe9ae32d9588ffc6ca9bf716
jjuanramos <jjuanramos91@gmail.com>
2023-09-25 18:02:15 +0200

    added lstclear and lstdelone

---
+++ ft_lstclear.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 17:56:51 by juramos           #+#    #+#             */
+/*   Updated: 2023/09/25 18:00:15 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*tmp;
+
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, (*del));
+		*lst = tmp;
+	}
+}
