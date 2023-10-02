d9fc1235599ff8da45e77b9ab451f586beacfe0c
Juan Ramos <juramos@c2r13s1.42madrid.com>
2023-10-02 14:10:54 +0200

    fixed strrchr

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:11:10 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/25 16:57:33 by juramos          ###   ########.fr       */
+/*   Updated: 2023/10/02 13:32:21 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ int	ft_lstsize(t_list *lst)
 
 	len = 0;
 	while (lst && ++len)
-	{
 		lst = lst->next;
-	}
 	return (len);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,9 +6,11 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 11:59:03 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/25 12:28:16 by juramos          ###   ########.fr       */
+/*   Updated: 2023/10/02 14:07:22 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -21,7 +23,20 @@ char	*ft_strrchr(const char *s, int c)
 			last = (char *)s;
 		s++;
 	}
-	if (!c)
+	if (!c || *s == (char)c)
 		last = (char *)s;
 	return (last);
 }
+/*
+#include <string.h>
+int	main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("value of strrchr: %s.\nvalue of ft_strrchr: %s.\n",
+			strrchr(argv[1], ft_atoi(argv[2])),
+			ft_strrchr(argv[1], ft_atoi(argv[2])));
+	}
+	return (0);
+}
+*/
