e2824a491d536c0cac12d5706322da28f2697932
Juan Ramos <juramos@c2r13s1.42madrid.com>
2023-10-02 11:40:49 +0200

    added more tests

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 18:11:44 by juramos           #+#    #+#             */
-/*   Updated: 2023/09/19 18:21:37 by juramos          ###   ########.fr       */
+/*   Updated: 2023/10/02 11:37:56 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	ret[pos] = '\0';
 	return (ret);
 }
+/*
+char	add_pos_to_char(unsigned int i, char c)
+{
+	return (c + i);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		printf("The outcome of adding 1 int to each char of %s i %s.\n",
+			argv[1], ft_strmapi(argv[1], &add_pos_to_char));
+	}
+}
+*/
