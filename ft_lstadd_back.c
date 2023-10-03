dc274e50edb4ecdbb6132d528364b4c88cdeef50
jjuanramos <jjuanramos91@gmail.com>
2023-10-03 18:58:03 +0200

    added more tests

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -22,3 +22,26 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	else
 		tmp->next = new;
 }
+/*
+int	main(void)
+{
+	char		*val;
+	char		*val2;
+	t_list	*t;
+	t_list	*t2;
+
+	val = "Goodbye";
+	t = ft_lstnew(val);
+	val2 = "Hello";
+	t2 = ft_lstnew(val2);
+	printf("value of t before: %s for content, %s for next.\n",
+		(char *)(t->content), (char *)(t->next));
+	printf("value of t2 before: %s for content, %s for next.\n",
+		(char *)(t2->content), (char *)(t2->next));
+	ft_lstadd_back(&t, t2);
+	printf("value of t after: %s for content, %s for next.\n",
+		(char *)(t->content), (char *)((t->next)->content));
+	printf("value of t2 after: %s for content, %s for next.\n",
+		(char *)(t2->content), (char *)(t2->next));
+}
+*/
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -20,3 +20,38 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 		free(lst);
 	}
 }
+/*
+void	delone(void	*s)
+{
+	free(s);
+}
+
+int	main(void)
+{
+	char		*val;
+	t_list	*t;
+
+	val = "Goodbye";
+	t = ft_lstnew(val);
+	
+	// for us to be able to free a pointer,
+	// we must have allocated it first through malloc.
+	// so, it's better to use malloc + strcpy.
+	char *val2 = malloc(6);
+	strcpy(val2, "Hello");
+	t_list *t2 = ft_lstnew(val2);
+
+	printf("value of t before: %s for content, %s for next.\n",
+		(char *)(t->content), (char *)(t->next));
+	printf("value of t2 before: %s for content, %s for next.\n",
+		(char *)(t2->content), (char *)(t2->next));
+	ft_lstadd_back(&t, t2);
+	printf("value of t after: %s for content, %s for next.\n",
+		(char *)(t->content), (char *)((t->next)->content));
+	printf("value of t2 after: %s for content, %s for next.\n",
+		(char *)(t2->content), (char *)(t2->next));
+	ft_lstdelone(t2, delone);
+	printf("value of t after: %s for content, %s for next.\n",
+		(char *)(t->content), (char *)(t->next));
+}
+*/
